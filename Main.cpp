0bf0a724a42a529267cdfaca59087ec24920e8c5
Thodemar <thom-183@hotmail.com>
2022-03-23 11:07:55 +0100

    Hello world

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Thodemar on 23-3-2022.
+//
+// "Success is not final, failure is not fatal: it is the courage to continue that counts"
+//
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello world";
+}
