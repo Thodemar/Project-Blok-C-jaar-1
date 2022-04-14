bc228f3a4425b7299bb69a639f685436e7c64348
Thodemar <thom-183@hotmail.com>
2022-04-14 11:22:05 +0200

    Commented the shit out off the code

--- Main.ino
+++ Main.ino
@@ -18,271 +18,232 @@ const int sonsorMiddleRight = 17; // a3
 const int sonsorRightRight = 18; // a4
 
 
-
 long duration, distance, RightSensor,BackSensor,FrontSensor,LeftSensor;
 
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(motorLd, OUTPUT);
-  pinMode(motorRd, OUTPUT);
-  pinMode(motorLs, OUTPUT);
-  pinMode(motorRs, OUTPUT);
-  pinMode(echoPinR, INPUT);
-  pinMode(trigPinR, OUTPUT);
-  pinMode(trigPinL, OUTPUT);
-  pinMode(echoPinL, INPUT);
-  pinMode(trigPinF, OUTPUT);
-  pinMode(echoPinF, INPUT);
-  pinMode(fan, OUTPUT);
-  Serial.begin(9600);
+    // put your setup code here, to run once:
+    pinMode(motorLd, OUTPUT);
+    pinMode(motorRd, OUTPUT);
+    pinMode(motorLs, OUTPUT);
+    pinMode(motorRs, OUTPUT);
+    pinMode(echoPinR, INPUT);
+    pinMode(trigPinR, OUTPUT);
+    pinMode(trigPinL, OUTPUT);
+    pinMode(echoPinL, INPUT);
+    pinMode(trigPinF, OUTPUT);
+    pinMode(echoPinF, INPUT);
+    pinMode(fan, OUTPUT);
+//  Serial.begin(9600);
 }
 
 
-
-int SonarSensor(int trigPin,int echoPin)
-{
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-duration = pulseIn(echoPin, HIGH);
-distance = (duration/2) / 29.1;
-//Serial.print(distance);
-//Serial.print("\n");
-delay(100);
-return distance;
+// function to read a distance from a HC-SR04
+int SonarSensor(int trigPin,int echoPin){
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration/2) / 29.1;
+    delay(100);
+    return distance;
 }
 
 
-
+// Makes to power over the H Bridge go forwards
 void vooruit(){
-  digitalWrite(motorRd, LOW);
-  analogWrite(motorRs, 220);
-  digitalWrite(motorLd, LOW);
-  analogWrite(motorLs, 250);
+    digitalWrite(motorRd, LOW);
+    analogWrite(motorRs, 220);
+    digitalWrite(motorLd, LOW);
+    analogWrite(motorLs, 250);
 
 }
 
 
-
+// Makes to power over the H Bridge go backwards
 void achterruit(){
-  digitalWrite(motorRd, HIGH);
-  analogWrite(motorRs, 35);
-  digitalWrite(motorLd, HIGH);
-  analogWrite(motorLs, 5);
+    digitalWrite(motorRd, HIGH);
+    analogWrite(motorRs, 35);
+    digitalWrite(motorLd, HIGH);
+    analogWrite(motorLs, 5);
 
 }
 
-
+// Makes the moters go stop
 void stopgoing(){
-  digitalWrite(motorRd, HIGH);
-  analogWrite(motorRs, 255);
-  digitalWrite(motorLd, HIGH);
-  analogWrite(motorLs, 255);
+    digitalWrite(motorRd, HIGH);
+    analogWrite(motorRs, 255);
+    digitalWrite(motorLd, HIGH);
+    analogWrite(motorLs, 255);
 
-  }
+}
 
-
+// Returns a 'r' or 'l' depending on which side there is the most room
 char whatDirection(){
-  RightSensor = SonarSensor(trigPinR, echoPinR);
-  LeftSensor = SonarSensor(trigPinL, echoPinL);
-  if(LeftSensor > RightSensor){
-    return 'l';
-//    Serial.print("hoi\n");
+    RightSensor = SonarSensor(trigPinR, echoPinR);
+    LeftSensor = SonarSensor(trigPinL, echoPinL);
+    if(LeftSensor > RightSensor){
+        return 'l';
+    }
 
-  }
-  if(LeftSensor < RightSensor){
-    return 'r';
-//      Serial.print("hoi\n");
+    if(LeftSensor < RightSensor){
+        return 'r';
+    }
 
-  }
-   if(LeftSensor == RightSensor){
-    return 'r';
-//    Serial.print("hoi\n");
+    if(LeftSensor == RightSensor){
+        return 'r';
+    }
 
-  }
-  
 }
-   
 
 
+// Turns 1 moter backwards and the other one forwards based on what direction it is safe to go towards
 void turn(int ms){
-  char direction = whatDirection();
-  delay(100);
+    //  Gets the safest direction to turn to from the function whatDirection()
+    char direction = whatDirection();
+    delay(100);
 
 
-  
-  if(direction == 'l'){
-    digitalWrite(motorRd, LOW);
-    analogWrite(motorRs, 220);
-    digitalWrite(motorLd, HIGH);
-    analogWrite(motorLs, 5);
-    delay(ms);
-    stopgoing();  
-  }
-  if(direction == 'r'){
-    digitalWrite(motorRd, HIGH);
-    analogWrite(motorRs, 35);
-    digitalWrite(motorLd, LOW);
-    analogWrite(motorLs, 250);
-    delay(ms);
-    stopgoing();
-  }
+    if(direction == 'l'){
+        digitalWrite(motorRd, LOW);
+        analogWrite(motorRs, 220);
+        digitalWrite(motorLd, HIGH);
+        analogWrite(motorLs, 5);
+        delay(ms);
+        stopgoing();
+    }
+    if(direction == 'r'){
+        digitalWrite(motorRd, HIGH);
+        analogWrite(motorRs, 35);
+        digitalWrite(motorLd, LOW);
+        analogWrite(motorLs, 250);
+        delay(ms);
+        stopgoing();
+    }
 }
 
-
+// Angles the robot towards the heat source
 void angel(char direction){
-  int ms = 150;
-//  char direction = 'r';
-  delay(100);
+    int ms = 150;
+    delay(100);
 
 
-  
-  if(direction == 'l'){
-    digitalWrite(motorRd, LOW);
-    analogWrite(motorRs, 220);
-    digitalWrite(motorLd, HIGH);
-    analogWrite(motorLs, 5);
-    delay(ms);
-    stopgoing();  
-  }
-  if(direction == 'r'){
-    digitalWrite(motorRd, HIGH);
-    analogWrite(motorRs, 35);
-    digitalWrite(motorLd, LOW);
-    analogWrite(motorLs, 250);
-    delay(ms);
-    stopgoing();
-  }
+
+    if(direction == 'l'){
+        digitalWrite(motorRd, LOW);
+        analogWrite(motorRs, 220);
+        digitalWrite(motorLd, HIGH);
+        analogWrite(motorLs, 5);
+        delay(ms);
+        stopgoing();
+    }
+    if(direction == 'r'){
+        digitalWrite(motorRd, HIGH);
+        analogWrite(motorRs, 35);
+        digitalWrite(motorLd, LOW);
+        analogWrite(motorLs, 250);
+        delay(ms);
+        stopgoing();
+    }
 }
 
-
+// Checks if the heat source is infront of the robot and if it is close to the source
 int infrontFire(){
-  int readingLeft = analogRead(sonsorLeftLeft);
-  int readingMiddleleft = analogRead(sonsorMiddleLeft);
-  int readingMiddle = analogRead(sonsorCenter);
-  int readingMiddleright = analogRead(sonsorMiddleRight);
-  int readingRight = analogRead(sonsorRightRight);
-  if ((readingLeft < 200) and (readingMiddleleft < 200) and (readingRight < 200) and (readingMiddleright < 200 ) and (readingMiddle < 200)){
-//    Serial.print("Nee\n");
-    return -1;
-  }
-  else if ((readingLeft > readingMiddle) or (readingMiddleleft > readingMiddle) or (readingRight > readingMiddle) or (readingMiddleright > readingMiddle)){
-//    Serial.print("Nee\n");
-    return 0;
-  } else {
-    Serial.print(readingMiddle);
-    Serial.print("\n");
-    if (readingMiddle > 700){
-      return 2;
-    } else {
-//      Serial.print("Ja\n");
-      return 1;
+    int readingLeft = analogRead(sonsorLeftLeft);
+    int readingMiddleleft = analogRead(sonsorMiddleLeft);
+    int readingMiddle = analogRead(sonsorCenter);
+    int readingMiddleright = analogRead(sonsorMiddleRight);
+    int readingRight = analogRead(sonsorRightRight);
+
+    // if it cannot find a heat source
+    if ((readingLeft < 200) and (readingMiddleleft < 200) and (readingRight < 200) and (readingMiddleright < 200 ) and (readingMiddle < 200)){
+        return -1;
     }
-  }
+
+        // if the middle is not the highest reading
+    else if ((readingLeft > readingMiddle) or (readingMiddleleft > readingMiddle) or (readingRight > readingMiddle) or (readingMiddleright > readingMiddle)){
+        return 0;
+    }
+
+        // the middle is the highest reading
+    else {
+        // the heat source is nearby
+        if (readingMiddle > 700){
+            return 2;
+        }
+            // heat source is not nearby
+        else {
+            return 1;
+        }
+    }
 }
 
-
+// Fan goes on for 8 seconds
 void bigFan(){
-  digitalWrite(fan, HIGH);
-  delay(8000);
-  digitalWrite(fan, LOW);
+    digitalWrite(fan, HIGH);
+    delay(8000);
+    digitalWrite(fan, LOW);
 
 }
 
+// decides which way the car needs to angle and how much
 void turnToFlame(){
-  int readingLeft = analogRead(sonsorLeftLeft);
-  int readingMiddleleft = analogRead(sonsorMiddleLeft);
-  int readingMiddle = analogRead(sonsorCenter);
-  int readingMiddleright = analogRead(sonsorMiddleRight);
-  int readingRight = analogRead(sonsorRightRight);
-  
-  if (readingLeft > readingRight){
-    if (readingLeft > readingMiddleleft){
-      angel('l');
-      angel('l');
-    }
-    else if (readingLeft < readingMiddleleft){
-      angel('l');
-    }
-  }
+    int readingLeft = analogRead(sonsorLeftLeft);
+    int readingMiddleleft = analogRead(sonsorMiddleLeft);
+    int readingMiddle = analogRead(sonsorCenter);
+    int readingMiddleright = analogRead(sonsorMiddleRight);
+    int readingRight = analogRead(sonsorRightRight);
 
-  else if (readingRight > readingLeft){
-    if (readingRight > readingMiddleright){
-      angel('r');
-      angel('r');
+    if (readingLeft > readingRight){
+        if (readingLeft > readingMiddleleft){
+            angel('l');
+            angel('l');
+        }
+        else if (readingLeft < readingMiddleleft){
+            angel('l');
+        }
     }
-    else if (readingRight < readingMiddleright){
-      angel('r');
+
+    else if (readingRight > readingLeft){
+        if (readingRight > readingMiddleright){
+            angel('r');
+            angel('r');
+        }
+        else if (readingRight < readingMiddleright){
+            angel('r');
+        }
     }
-  }
 }
 
 void loop() {
-// put your main code here, to run repeatedly:
-//  vooruit();
-//  delay(1000);
-//  stopgoing();
-//  delay(1000);
-//SonarSensor(trigPin1, echoPin1);
-//RightSensor = distance;
-//SonarSensor(trigPin2, echoPin2);
-//LeftSensor = distance;
-//SonarSensor(trigPinF, echoPinF);
-//FrontSensor = distance;
 
-//turn('r', 4);
-//delay(1000);
+    // if all is good car can drive
+    while (((infrontFire() == 1) or (infrontFire() == -1 ) or (infrontFire() == 2)) and (SonarSensor(trigPinF, echoPinF) > 20) ){
+        vooruit();
+    }
 
+    stopgoing();
 
+    // Is near flame so trigger fan
+    if((SonarSensor(trigPinF, echoPinF) <= 25) and (infrontFire() == 2)){
+        bigFan();
+    }
 
+        // is not going straight at the Flane
+    else if(infrontFire() == 0){
+        turnToFlame();
 
+    }
 
-while (((infrontFire() == 1) or (infrontFire() == -1 ) or (infrontFire() == 2)) and (SonarSensor(trigPinF, echoPinF) > 20) ){
-    vooruit();
-}
-
-stopgoing();
-
-// Is near flame so trigger fan
-if((SonarSensor(trigPinF, echoPinF) <= 25) and (infrontFire() == 2)){
-  bigFan();
-}
-
-// is not going straight at the Flane
-else if(infrontFire() == 0){
-  turnToFlame();
-  
-}
-
-// Is close to a object so need to turn otherwise it would hit the object
-else if(SonarSensor(trigPinF, echoPinF) <= 20 ){
-  achterruit();
-  delay(500);
-  turn(400);
-  stopgoing();
-  delay(100);
-} 
-
-
-
-//angel();
-//delay (200);
-
-//if(SonarSensor(trigPinF, echoPinF) <= 20){
-//  stopgoing();
-//  delay(100);
-//  turn(4);
-//  delay(1000);
-//  Serial.print("hoi\n");
-//
-//  }else{
-//  vooruit();
-//  
-//  }
-//delay(60);
-//Serial.print("hoi\n");
+        // Is close to a object so need to turn otherwise it would hit the object
+    else if(SonarSensor(trigPinF, echoPinF) <= 20 ){
+        achterruit();
+        delay(500);
+        turn(400);
+        stopgoing();
+        delay(100);
+    }
 
 }
